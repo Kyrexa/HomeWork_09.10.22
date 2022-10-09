918ab7841c7150b3dd40d6e78972003f1298272e
kyrexa <arzamas.orko@yandex.ru>
2022-10-09 16:58:34 +0300

    Сделал домашнее задание 3 семинара

---
+++ HomeWork_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу которая на вход принимает 5 число и проверяет, является ли оно палиндрамом.
+Console.Clear();
+Console.WriteLine("Напишите число: ");
+string n = Console.ReadLine();
+int len = n.Length;
+if ( len == 5)
+{
+    if(n[0] == n[4] && n [1] == n[3])
+    {
+        Console.WriteLine($"{n} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{n} - Не палиндром"); 
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {n} - Не пятизначное число"); 
+}
---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве. 
+Console.Clear();
+Console.Write("Напишите координату A точки 1: ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите координату B точки  точки 1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите координату C точки  точки 1: ");
+int c1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Напишите координату A точки 2: ");
+int a2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите координату B точки  точки 2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите координату C точки  точки 2: ");
+int c2 = Convert.ToInt32(Console.ReadLine());
+
+int A = a2 - a1;
+int B = b2 - b1;
+int C = c2 - c1;
+
+double length = (Math.Round(Math.Sqrt(A * A + B * B + C * C), 3)); // + Использовал Math.Round 
+Console.WriteLine($"Расстояние между точками {length}");
